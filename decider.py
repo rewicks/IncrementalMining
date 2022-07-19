b27c89ada2aebbed84b67a8d17e332300cc8e656
Anthony Aue <anthaue@microsoft.com>
2022-07-19 15:11:10 -0700

    inverse count decider

--- decider.py
+++ decider.py
@@ -10,3 +10,19 @@ class Decider:
         val = counts[i] / sum
         ret.append(val)
     return ret
+
+# Compute inverse counts (total - my count) for each input count. This is our numerator.
+# Sum those to get a new denominator.
+class InverseCountDecider(Decider):
+    def Decide(self, counts):
+        # Assumption: final count is "other", and we don't care about it.
+        my_counts=counts[:-1]
+        total=sum(my_counts)
+        diffs=[total - c for c in my_counts]
+        diffsum = sum(diffs)
+        ret = [d / diffsum for d in diffs]
+        # Make sure we're returning something that looks like a probability distribution
+        assert(abs(1.0 - sum(ret)) < .01)
+        return ret
+
+
