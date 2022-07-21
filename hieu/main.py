4a3afbccbdc58c0fbdb331151a86ad9654dec8bc
Hieu Hoang <hihoan@microsoft.com>
2022-07-21 08:03:25 -0700

    restart

--- hieu/main.py
+++ hieu/main.py
@@ -3,20 +3,97 @@ import os
 import sys
 import argparse
 import numpy as np
+from collections import Counter
 
 sys.path.append("..")
 from utils import MySQL, Languages
 from environment import Env, GetEnv, Dummy
+from state import Link, MonolingualDocument, ParallelDocument
 
-class State():
+class State2():
+    def __init__(self, languages, link_queue_limit):
+
+        # unless people protest I think a list of size k is
+        # actually simpler than a queue
+        self.link_queue = []
+        self.link_queue_limit = link_queue_limit
+        self.visited_links = set()
+
+        # these actually might be *too* large to keep, so maybe
+        # just keep metadata from them
+        self.monolingual_documents = []
+        self.parallel_documents = []
+
+        self.languages = languages
+
+    def add_link(self, link):
+        if link.url not in self.visited_links:
+            self.link_queue.append(link)
+        if len(self.link_queue) > self.link_queue_limit:
+            self.link_queue.pop(0)
+
     def get_features(self):
-        ret = np.zeros([3])
-        return ret
+        # monolingual documents
+        doc_targeted_language_counter = Counter([x.language for x in self.monolingual_documents if x.language in self.languages])
+        doc_targeted_langs = [doc_targeted_language_counter[lang] for lang in self.languages]
+        doc_non_targeted_language_counter = Counter([x.language for x in self.monolingual_documents if x.language not in self.languages])
+        doc_non_targeted_langs = [sum(doc_non_targeted_language_counter)]
 
+        # link counters
+        link_targeted_language_counter = Counter([x.language for x in self.link_queue if x.language in self.languages])
+        link_targeted_langs = [link_targeted_language_counter[lang] for lang in self.languages]
+        link_non_targeted_language_counter = Counter([x.language for x in self.link_queue if x.language not in self.languages])
+        link_non_targeted_langs = [sum(link_non_targeted_language_counter)]
+
+        return doc_targeted_langs + doc_non_targeted_langs + link_targeted_langs + link_non_targeted_langs
+
+def create_start_state_from_node(root, languages, link_queue_limit):
+    state = State2(languages, link_queue_limit=link_queue_limit)
+
+    # update with the only crawled page
+    root_document = MonolingualDocument(docid=root.urlId, langid=root.lang)
+    state.monolingual_documents.append(root_document)
+
+    # add all children
+    for li in root.links:
+        link = Link(link_text=li.text,
+                    link_text_lang=li.textLang,
+                    link_url=li.childNode.url,
+                    link_url_id=li.childNode.urlId)
+        state.add_link(link)
+
+    return state
+######################################################################################
+def SafeDiv(a, b):
+    if b == 0:
+        return 10
+    else:
+        return a / b
+
+def CalcProbs(state, coefficients):
+    features = np.array(state.get_features())
+    print("features", features)
+
+    sumDocs = features[0] + features[1] + features[2]
+    sumLinks = features[3] + features[4] + features[5]
+
+    scores = np.empty(features.shape)
+    scores[0] = SafeDiv(sumDocs, features[0])
+    scores[1] = SafeDiv(sumDocs, features[1])
+    scores[2] = SafeDiv(sumDocs, features[2])
+
+    scores[3] = SafeDiv(sumLinks, features[3]) 
+    scores[4] = SafeDiv(sumLinks, features[4])
+    scores[5] = SafeDiv(sumLinks, features[5])
+    print("scores", scores)
+
+
+    
 ######################################################################################
 def main(args):
     print("Starting")
-    maxStep = 1000000
+    maxStep = 2 #000000
+    coefficients = [1, 2, 3, 4, 5, 6]
 
     sqlconn = MySQL(args.config_file)
     languages = Languages(sqlconn)
@@ -24,11 +101,10 @@ def main(args):
     # env = Dummy()
     env = GetEnv(args.config_file, languages, args.host_name)
 
-    state = State()
+    state = create_start_state_from_node(env.rootNode, langIds, args.linkQueueLimit)
 
     for t in range(1, maxStep):
-        features = np.array(state.get_features())
-        print("features", features)
+        CalcProbs(state, coefficients)
 
 
     print("Finished")
@@ -38,6 +114,7 @@ if __name__ == "__main__":
     parser.add_argument('--config-file', default="../config.ini")
     parser.add_argument('--host-name', default="http://www.visitbritain.com/")
     parser.add_argument('--lang-pair', default="en-fr")
+    parser.add_argument('--link-queue-limit', dest="linkQueueLimit", type=int, default=10000000, help="Maximum size of buckets of links")
 
     args = parser.parse_args()
     #print("cpu", args.cpu)
