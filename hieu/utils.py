4d190c024ebfa3b5a4083f30a25e3eba63106ecc
Hieu Hoang <hieuhoang@gmail.com>
2022-07-21 17:12:39 -0700

    testing edit distance and strand match

--- hieu/utils.py
+++ hieu/utils.py
@@ -39,3 +39,15 @@ def StrNone(arg):
         return "None"
     else:
         return str(arg)
+
+# Given a candidate url, return the index of the item in existing_urls with the smallest string edit distance.
+def FindStrandMatch(candidate_url, existing_urls):
+    distances = [editdistance.eval(candidate_url, curr) for curr in existing_urls]
+    min_dist = sys.maxsize 
+    min_dist_idx = -1
+    for i in range(len(distances)):
+        if distances[i] < min_dist:
+            min_dist = distances[i]
+            min_dist_idx = i
+    return min_dist_idx
+
